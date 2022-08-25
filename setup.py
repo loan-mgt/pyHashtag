44b18827d1c7b5094e2572745d0eb397392ff4f1
qypol342 <qypol342@gmail.com>
2022-08-25 22:21:45 +0200

    file moved

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Hashtag to text'
+LONG_DESCRIPTION = 'The package is made for twitter, it add hashtag to know word in text'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="Hashtag", 
+        version=VERSION,
+        author="Loan MAEGHT",
+        author_email="qypol342@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['unidecode','requests','rich'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'hashtag'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Linux",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
